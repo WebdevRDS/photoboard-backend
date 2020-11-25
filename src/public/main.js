ee26f7de7d3b3d9b8795fdf3f907b6b48ab166ef
Gayduk Dmitry <webdev.rds@gmail.com>
2020-11-25 10:13:01 +0200

    update frontend build

--- src/public/main.js
+++ src/public/main.js
@@ -44,7 +44,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    api: 'http://localhost:4201/api',
+    api: 'https://photoboard-backend.herokuapp.com/api',
 };
 /*
  * For easier debugging in development mode, you can import the following file
